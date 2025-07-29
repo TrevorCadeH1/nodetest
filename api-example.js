10176eb7164ae4ba8cd7559ffdec96a635ae9e0f
Trevor Hutchison <trevor.hutchison@wurthbsc.com>
2025-07-29 11:03:39 -0500

    update

--- api-example.js
+++ api-example.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 3004;
+const port = process.env.PORT || 3004;
 
 // Middleware to enable CORS for your website
 app.use(cors({
